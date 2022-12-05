5e789d9fe1af0a05953203863aa7f041716b5a53
jonro2955 <jonro.2955@gmail.com>
2022-12-05 12:54:50 -0800

    Complete #5

--- 5_maximumsubarray.py
+++ 5_maximumsubarray.py
@@ -1,31 +1,54 @@
 class Solution(object):
-    """
-    https://leetcode.com/problems/maximum-subarray/
-
-    Given an integer array/list nums, find the subarray which has the largest sum and return its
-    sum.
-
-    Example Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-    Output: 6
-    Explanation: [4,-1,2,1] has the largest sum = 6.
-
-    Example Input: nums = [1]
-    Output: 1
-
-    Example Input: nums = [5,4,-1,7,8]
-    Output: 23
-
-    Constraints:
-    1 <= nums.length <= 105
-    -104 <= nums[i] <= 104
-
-    Note:
-    If you have figured out the O(n) solution, try coding another solution using the divide and
-    conquer approach.
-    """
     def maxSubArray(self, nums):
+        for i in range(1, len(nums)):
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1]
+        return max(nums)
+
         """
+        https://leetcode.com/problems/maximum-subarray/
         :type nums: List[int]
         :rtype: int
+
+        Need to evaluate all possible subsets, calculate their sums and find the max among them.
+
+        The subsets themselves aren't important. Their sums are. Even though the problem says 
+        find 'subarray', it tells you to return a single sum. So it is more productive to focus on
+        the return value rather than fixate on a subarrray.
+
+        Algorithm:
+
+        "Go through each item in the list, and only if the item before it is greater than 0,
+        then change the current item to be the sum of it and its preceding item."
+        
+        This makes each number in the array become the sum of the subset before it that produces an 
+        increase in value. 
+
+        If a preceding number is negative, adding it will decrease the value of the current 
+        subarray sum, so we need to ignore that to keep the higher sum.
+        
+        After all iterations are complete, pick the maximum value from the array and 
+        return it"
+
+        This solution covers cases where all array values are negative. In such cases, there are
+        no subarrays to sum up because any subarray will produce an even lower sum than the
+        array values themselves. So we can just pick the max among the negative values.
+
+        In these types of questions where you are trying to find a continuous subset's sum (or
+        product) that evauates to the highest value among all possible subsets, the general
+        rule is that only those numbers greater than 0 that precedes each number will
+        increase the subset's sum (or >1 in the case of a product) so only in those cases you should
+        combine the preceding subset with the current array item to evaluate a larger subset
+        sum/product. If the preceding value will decrease the current subset's evaluation, then the
+        current item becomes the start of a new subset to consider.
+
+        [-2, 1,-3, 4,-1, 2, 1,-5, 4] =>
+        [-2, 1,-2, 4, 3, 5, 6, 1, 5] => The max, 6, is the sum of subset [4,-1, 2, 1]
+
+        In code, we cannot evaluate the preceding item of the first item of the array,
+        so we start with the second item at index 1.
         """
+
+
+
 
